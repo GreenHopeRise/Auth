7d6b7f37ea093170dce3e38488475348205100e0
GreenHopeRise <khalidanwarantur96@gmail.com>
2026-05-07 23:59:44 +0600

    do

--- src/auth/AuthContext.jsx
+++ src/auth/AuthContext.jsx
@@ -1,15 +1,35 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
-const AuthContext = createContext()
-export const AuthProvider =({children})=>{
-    const [user, setUser] = useState(null)
-    const [token, setToken] = useState(null)
-    const [loading, setLoading] = useState(true)
-    const isAuthenticated =!!token
-    return(
-        <AuthContext.Provider value={{user, token, loading, }}>
-            {children}
 
-        </AuthContext.Provider>
-    )
-}
+const AuthContext = createContext();
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const isAuthenticated = !!token;
+  const login = (token, userData)=>{
+    localStorage.setItem('token',token)
+    setToken(token)
+    setUser(userData)
+  }
+  const logout = ()=>{
+    localStorage.removeItem('token')
+    setToken(null)
+    setUser(null)
+  }
+  useEffect(()=>{
+    const savedToken = localStorage.getItem('token')
+    if(savedToken){
+        setToken(savedToken)
+        setUser({name:'demo user'})
+
+    }
+  },[])
+  return (
+    <AuthContext.Provider value={{ user, token, loading, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = ()=> useContext(AuthContext)
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+    const navigate = useNavigate()
+    const handleLogin = ()=>{
+        const fakeToken ='123abc'
+        const fakeUser = 'khalid'
+        log
+    }
   return (
     <div>
         <input type='email' />
         <input type="password" />
-        <button>Login</button>
+        <button onClick={handleLogin}>Login</button>
     </div>
   )
 }
